beb4e75281a60e63dd6321443142f32b67947511
maurycywoznica <mjw11@wp.pl>
2022-05-30 23:16:38 +0200

    posts models

---
+++ posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Posts(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=250)
+    content = models.CharField()
+    tag_is = models.ForeignKey(Tags.id)
+
+class Tags(models.Model):
+    id = models.AutoField(primary_key=True)
