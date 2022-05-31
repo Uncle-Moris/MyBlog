44e767f8a55b855be71c1cca961336717438c96e
maurycywoznica <mjw11@wp.pl>
2022-05-31 23:01:11 +0200

    some things

--- posts/models.py
+++ posts/models.py
@@ -5,6 +5,7 @@ class Posts(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=250)
     content = models.CharField()
+    extras = models.JSONField(jsonb=True)
     tag_is = models.ForeignKey(Tags.id)
 
 
