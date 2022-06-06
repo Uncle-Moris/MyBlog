1c5920061da65bcfd186b885524c5217dbb9f43c
maurycywoznica <mjw11@wp.pl>
2022-06-06 23:34:23 +0200

    add created_at in to Post model

--- my_blog/posts/models.py
+++ my_blog/posts/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
