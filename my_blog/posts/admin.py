c9ad3b173f57d0d45358d88955126b120d54caeb
maurycywoznica <mjw11@wp.pl>
2022-06-09 23:35:01 +0200

    admin register posts

--- my_blog/posts/admin.py
+++ my_blog/posts/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at']
+    list_filter = []
+
