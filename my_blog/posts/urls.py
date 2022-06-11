5fdb35307da657d372746efbe055b7b623f68ce5
maurycywoznica <mjw11@wp.pl>
2022-06-12 00:46:25 +0200

    list root fix

--- my_blog/posts/urls.py
+++ my_blog/posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import posts_list, posts_details
 
 urlpatterns = [
-    path('list', posts_list),
+    path('list/', posts_list),
     path('<int:post_id>', posts_details)
 ]
