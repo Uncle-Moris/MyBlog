d1a7c2c5f2cbaf9a667e1d878e67d71ca7d7df04
maurycywoznica <mjw11@wp.pl>
2022-06-19 22:31:48 +0200

    add tags field in posts mosel

--- my_blog/posts/models.py
+++ my_blog/posts/models.py
@@ -5,3 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField('tags.Tag', related_name='posts')
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
