6d764807a4bac4907d28dcded0f75edfcc3430a1
maurycywoznica <mjw11@wp.pl>
2022-06-27 21:37:47 +0200

    tags listing in posts list without searching

--- my_blog/posts/views.py
+++ my_blog/posts/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Post
+from tags.models import Tag
 
 
 def posts_list(request):
     posts_list = Post.objects.all()
+    tags_list = Tag.objects.all()
     q = request.GET.get("title")
-    if q:
-        posts_list = posts_list.filter(title__icontains=q)
-    context = {'posts_list': posts_list}
+    tag = request.GET.get("tags")
+    if q or tag:
+        posts_list = posts_list.filter(title__icontains=q, tags=tag.name.get(tag))
+
+
+    context = {'posts_list': posts_list, 'tags_list': tags_list}
     return render(request,'posts/list.html', context)
 
 
--- my_blog/tags/models.py
+++ my_blog/tags/models.py
@@ -12,4 +12,4 @@ class Tag(models.Model):
         return super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.name} {self.slug}"
+        return f"{self.name}"
