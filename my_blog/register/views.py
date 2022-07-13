5cfcd1c4c1449c9893c48e1b8001ebd6b45c6ac0
maurycywoznica <mjw11@wp.pl>
2022-07-13 22:44:06 +0200

    rooting for the register app

--- my_blog/register/views.py
+++ my_blog/register/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from .forms import RegisterForm
+
+
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+        return redirect(reverse('home'))
+    else:
+        form = RegisterForm()
+    return render(response, "accounts/register.html", {"form": form})
 
 # Create your views here.
