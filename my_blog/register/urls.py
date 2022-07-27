84e35dbb3661c953d2477edf65c12bef9e610968
maurycywoznica <mjw11@wp.pl>
2022-07-27 22:51:11 +0200

    login page

--- my_blog/register/urls.py
+++ my_blog/register/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register_request
+from .views import register_request, login_request
 
 app_name = "register"
 urlpatterns = [
-    path('register/', register_request, name="register")
+    path('register/', register_request, name="register"),
+    path("login/", login_request, name="login")
 ]
