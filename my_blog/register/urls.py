01bb1a2377098bec388535704e38a05cfad853ce
maurycywoznica <mjw11@wp.pl>
2022-07-30 17:02:20 +0200

    logout

--- my_blog/register/urls.py
+++ my_blog/register/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register_request, login_request
+from .views import register_request, login_request, logout_request
 
 app_name = "register"
 urlpatterns = [
     path('register/', register_request, name="register"),
-    path("login/", login_request, name="login")
+    path('login/', login_request, name="login"),
+    path('logout/', logout_request, name= "logout")
 ]
