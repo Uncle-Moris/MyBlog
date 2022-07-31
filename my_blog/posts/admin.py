cfde548ed962df8b8a4f119557c21eaf8eb1708d
maurycywoznica <mjw11@wp.pl>
2022-07-31 21:56:00 +0200

    Newp post model migration

--- my_blog/posts/admin.py
+++ my_blog/posts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
+
 @admin.register(Post)
 class PostsAdmin(admin.ModelAdmin):
     list_display = ['id', 'title']
     list_filter = []
-
+    preserve_filters = {'slug': ('title',)}
