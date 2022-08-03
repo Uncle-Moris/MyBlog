9e6fdc04d8d3ceb76a73f5ac39f9a64856a85eaa
maurycywoznica <mjw11@wp.pl>
2022-08-03 22:40:36 +0200

    sad js

--- my_blog/main/static/main/js/main.js
+++ my_blog/main/static/main/js/main.js
@@ -1,4 +1,19 @@
-let x = 5
-if (x => 4){
-    console.log(`bravo`)
+let x = 5;
+if (x == 4){
+    console.log(`bravooo`)
+};
+
+const newObjectList = {
+    'name':'test'
 }
+
+alert(newObjectList['name'])
+
+const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+
+let text = "";
+for (let i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
