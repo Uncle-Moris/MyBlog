cdca097cc2546034d04d889f481ba31f496f0970
maurycywoznica <mjw11@wp.pl>
2022-08-04 22:15:54 +0200

    new post styles

--- my_blog/main/static/main/js/main.js
+++ my_blog/main/static/main/js/main.js
@@ -1,19 +1,3 @@
 let x = 5;
-if (x == 4){
-    console.log(`bravooo`)
-};
-
-const newObjectList = {
-    'name':'test'
-}
-
-alert(newObjectList['name'])
-
-const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
-
-let text = "";
-for (let i = 0; i < cars.length; i++) {
-  text += cars[i] + "<br>";
-}
 
 document.getElementById("demo").innerHTML = text;
