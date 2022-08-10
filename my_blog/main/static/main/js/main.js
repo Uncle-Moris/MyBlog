04d013c152ace1315d2dc7a1e0726a7b8e6969ce
maurycywoznica <mjw11@wp.pl>
2022-08-10 22:48:40 +0200

    js undergang

--- my_blog/main/static/main/js/main.js
+++ my_blog/main/static/main/js/main.js
@@ -1,19 +1,6 @@
-let abc = [1, 2, 3, 4, 5, 6];
+const alertBtn = document.querySelector('.alert-btn')
 
-
-for(let i = 0; i <= abc.length  ; i++ ) {
-    console.log(i)
+function testAlert () {
+    console.log('sdsadsa')
 }
-
-function something(arg1, arg2, arg3){
-    console.log(arg1+arg2+arg3)
-}
-
-something(1,2,3)
-
-if (abc.length>5){
-    console.log('TRUE')
-}
-else {
-    console.log('FALSE')
-}
+alertBtn.addEventListener('click', testAlert);
