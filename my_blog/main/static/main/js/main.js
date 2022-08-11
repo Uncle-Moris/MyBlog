33eb844d5ee7bc580057ce6e6b9b17677847d65a
maurycywoznica <mjw11@wp.pl>
2022-08-11 23:10:50 +0200

    major js fix XDDDD

--- my_blog/main/static/main/js/main.js
+++ my_blog/main/static/main/js/main.js
@@ -1,11 +1,24 @@
 const alertBtn = document.querySelector('.alert-btn')
+let sqB = document.querySelector('.sq-btn')
+let square = document.querySelector('.square')
 
-function testAlert () {
-    console.log('sdsadsa')
-}
-alertBtn.addEventListener('click', testAlert);
 
-function someClass () {
-    alertBtn.classList('')
+
+if (sqB) {
+  sqB.addEventListener('click', () => {
+    console.log('btn clicked');
+  });
 }
-alertBtn.addEventListener('click', )
+
+sqB?.addEventListener('click', () => {
+  console.log('btn clicked');
+});
+
+
+
+function add () {
+    console.log(2 + 2)
+}
+
+
+sqB.addEventListener('click', add);
