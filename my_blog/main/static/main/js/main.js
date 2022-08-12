3a3dcdcf3ecdf019a207d7689e002afd30280c68
maurycywoznica <mjw11@wp.pl>
2022-08-12 23:18:00 +0200

    major js fix XDDDD

--- my_blog/main/static/main/js/main.js
+++ my_blog/main/static/main/js/main.js
@@ -3,22 +3,8 @@ let sqB = document.querySelector('.sq-btn')
 let square = document.querySelector('.square')
 
 
-
-if (sqB) {
-  sqB.addEventListener('click', () => {
-    console.log('btn clicked');
-  });
+function someClass () {
+    square.classList.toggle('red')
 }
 
-sqB?.addEventListener('click', () => {
-  console.log('btn clicked');
-});
-
-
-
-function add () {
-    console.log(2 + 2)
-}
-
-
-sqB.addEventListener('click', add);
+sqB.addEventListener('click', someClass);
